d4ab853b9603a2779fa1f783b184d71a08165a4b
odootest <odootest2023@163.com>
2023-10-09 18:33:56 +0800

    remove duplicate words

---
+++ remove-duplicate.js
@@ -0,0 +1,12 @@
+import _ from 'lodash'
+import fs from 'node:fs'
+
+const filename = 'words.txt'
+const content = fs.readFileSync(filename, 'utf-8')
+const words = content.split('\n')
+const duplicate_words = _.chain(words).countBy().toPairs().filter(d => d[1] > 1).value()
+const unique_words = _.uniq(words)
+console.log('before', words.length)
+console.log('duplicate', duplicate_words)
+console.log('after', unique_words.length)
+fs.writeFileSync(filename, unique_words.join('\n'))
